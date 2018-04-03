c4214e8c6a8d5ee6f5ae369ba4d61e00090eddc1
Jose Augusto Bolina <joseaugusto.bolina@hotmail.com>
2018-04-03 18:34:45 -0300

    change dir location

--- proj-1/run.sh
+++ proj-1/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xterm -hold -e python ~/Git/distributed-systems/proj-1/main_server.py &
-xterm -hold -e python ~/Git/distributed-systems/proj-1/main_client.py &
+xterm -hold -e python $PWD/main_server.py &
+xterm -hold -e python $PWD/main_client.py &
